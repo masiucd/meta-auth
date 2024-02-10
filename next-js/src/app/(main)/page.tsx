021d7bb0f0a70f729878e45e1bd6f3b871e2dd45
masiucd <ciszekmarcell@gmail.com>
2024-02-10 12:11:54 +0100

    Add login form and styling to main page

--- next-js/src/app/(main)/page.tsx
+++ next-js/src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {type InputHTMLAttributes, type ReactNode} from "react";
 
 import {PageWrapper} from "@/app/components/page-wrapper";
@@ -7,17 +8,20 @@ import {cn} from "../lib/cn";
 function FormGroup({
   children,
   label,
-  id,
   className,
 }: {
   children: ReactNode;
   label: string;
-  id: string;
   className?: string;
 }) {
+  let formatedLabel = label.replaceAll("-", " ").toLowerCase();
   return (
     <div className={cn("flex flex-col gap-1", className)}>
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={label}>
+        <span className="font-semibold capitalize text-gray-700">
+          {formatedLabel}
+        </span>
+      </label>
       {children}
     </div>
   );
@@ -39,19 +43,33 @@ function Input({
 
 export default function Home() {
   return (
-    <PageWrapper>
-      <div className="w-[30rem] border border-red-500">
+    <PageWrapper className="items-center justify-center">
+      <div className="w-[30rem] rounded-sm border p-10 text-base shadow-md">
+        <h3 className="mb-10 text-3xl font-bold text-gray-700">Login</h3>
         <form action="">
-          <fieldset>
-            <FormGroup label="Name" id="name">
-              <Input type="text" id="email" name="email" />
+          <fieldset className="flex flex-col gap-1">
+            <FormGroup label="email">
+              <Input type="email" id="email" name="email" required />
             </FormGroup>
-            <FormGroup label="Email" id="email">
-              <Input type="email" id="email" name="email" />
+            <FormGroup label="password" className="mb-4">
+              <Input type="password" id="password" name="password" required />
             </FormGroup>
+            <button
+              className="h-10 w-full rounded-md bg-gray-900 text-white transition-opacity duration-200 ease-in-out hover:opacity-75"
+              type="submit"
+            >
+              Login
+            </button>
           </fieldset>
-          <button type="submit">Login</button>
         </form>
+        <div className="py-2 text-center">
+          <Link
+            className="underline underline-offset-2 transition-colors duration-200 hover:text-primary-500"
+            href="/"
+          >
+            Forgot your password?
+          </Link>
+        </div>
       </div>
     </PageWrapper>
   );
