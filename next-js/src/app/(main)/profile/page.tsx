f426efa5c361dd5131fb495f1f233c2afdf54a9f
masiucd <ciszekmarcell@gmail.com>
2024-02-10 15:38:22 +0100

    Add logout functionality and authentication check to ProfilePage

--- next-js/src/app/(main)/profile/page.tsx
+++ next-js/src/app/(main)/profile/page.tsx
@@ -1,13 +1,31 @@
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+
 import {PageWrapper} from "@/app/components/page-wrapper";
 
-export default function ProfilePage() {
+import {logout} from "../actions";
+
+async function checkAuth() {
+  let loggedIn = cookies().get("session")?.value;
+  if (!loggedIn) {
+    return redirect("/login");
+  }
+}
+
+export default async function ProfilePage() {
+  await checkAuth();
   return (
     <PageWrapper>
       <h1>Profile</h1>
       <p>Welcome to your profile page</p>
       <p>Here you can see your personal information and update your settings</p>
-      <form action="">
-        <button type="submit">logout</button>
+      <form action={logout}>
+        <button
+          className="rounded bg-gray-900 px-2 py-1 font-bold capitalize text-gray-100 shadow-sm transition-opacity duration-150 hover:opacity-65"
+          type="submit"
+        >
+          logout
+        </button>
       </form>
     </PageWrapper>
   );
