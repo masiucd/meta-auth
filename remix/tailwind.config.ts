1b1ec4b5a082816a0bde4cdb5090964ba929209f
masiucd <ciszekmarcell@gmail.com>
2024-02-11 09:21:30 +0100

    Add custom colors to tailwind.config.ts

--- remix/tailwind.config.ts
+++ remix/tailwind.config.ts
@@ -1,9 +1,15 @@
 import type {Config} from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.fuchsia,
+        gray: colors.zinc,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
